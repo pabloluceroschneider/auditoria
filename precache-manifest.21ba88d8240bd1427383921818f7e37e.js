82f0a434e143a0bcb9039f7e2a236d559f38b98d
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-06-09 19:21:44 -0300

    Updates

---
+++ precache-manifest.21ba88d8240bd1427383921818f7e37e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48c92ca8c06f50a01201cad6ca97f43",
+    "url": "/auditandoetica/index.html"
+  },
+  {
+    "revision": "82d01f0e1a8827293446",
+    "url": "/auditandoetica/static/css/main.f210ca21.chunk.css"
+  },
+  {
+    "revision": "af8517158c17898b6c1c",
+    "url": "/auditandoetica/static/js/2.3cbf902f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/auditandoetica/static/js/2.3cbf902f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d01f0e1a8827293446",
+    "url": "/auditandoetica/static/js/main.4ed3ed75.chunk.js"
+  },
+  {
+    "revision": "9b6369249245285a5f8c",
+    "url": "/auditandoetica/static/js/runtime-main.3076872d.js"
+  },
+  {
+    "revision": "c9592cb88b1af920a3f369190532c808",
+    "url": "/auditandoetica/static/media/simbolo0.c9592cb8.png"
+  }
+]);
