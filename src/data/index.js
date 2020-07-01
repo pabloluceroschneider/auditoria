f2567e69e5f2d12ec3b135fbf839d32853fd6a47
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-07-01 00:41:08 -0300

    Mas filminas

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,7 @@ export default () => {
 		{
 			page: {
 				current: 1,
-				next: 103
+				next: 2
 			},
 			title: 'Auditando tu Ética',
 			body:{
@@ -164,7 +164,75 @@ export default () => {
 		},
 		{
 			page: {
-				back: 103,
+				back: 8,
+				current: 200,
+				next: 201
+			},
+			title: 'Decidiste dar una paso al costado',
+			body: {
+				type:'string',
+				content:'Le comunicaste al gerente tu decision de no participar.'+
+						'Te quedas con la consciencia tranquila. Pero no por mucho...'
+			}
+		},
+		{
+			page: {
+				back: 9,
+				current: 201,
+				next: 202
+			},
+			title: 'Decidiste dar una paso al costado',
+			body: {
+				type:'string',
+				content:'Tu jefe, insiste en el caso. Quiere alguien de confianza '+
+						'y de la empresa. No quieren alguien nuevo. "USTED y nadie mas que '+
+						'USTED puede firmar estos papeles" Te dicen, con un tono más agresivo.'
+
+			}
+		},
+		{
+			page: {
+				back: 201,
+				current: 202,
+				next: 203
+			},
+			title: 'Decidiste dar una paso al costado',
+			body: {
+				type:'string',
+				content:'La situación ya tiene otro ambiente. '+
+						'A pesar de todo, decidis seguir firme.'
+
+			}
+		},
+		{
+			page: {
+				back: 202,
+				current: 203,
+				next: 204
+			},
+			title: 'Decidiste dar una paso al costado',
+			body: {
+				type:'image',
+				content:'amenazas-internas.jpg'
+
+			}
+		},
+		{
+			page: {
+				back: 203,
+				current: 204,
+				next: 9
+			},
+			title: 'Comienzan las amenazas',
+			body: {
+				type:'string',
+				content:'La presión pasó a ser amenazas, que vinculan tu trabajo pero '+
+						'también la integridad de tu familia. Tenes que firmar esos papeles...'
+			}
+		},
+		{
+			page: {
+				back: 8,
 				current: 9,
 				next: 10
 			},
@@ -187,6 +255,27 @@ export default () => {
 						'Es por ello que consideramos que las reflexiones sobre la ética y dilemas tales como el del Tranvía '+
 						'nos exigen a ejercitar nuestro moral inconsciente, que al final de todo, es en este tipos '+
 						'de situaciones en donde se ve verdaderamente qué tipo de personas somos.'
+			}
+		},
+		{
+			page: {
+				back: 10,
+				current: 11,
+				next: 12
+			},
+			title: '',
+			body: {
+				type:'string',
+				content: 'El auditor siempre debe emplear sus'+
+				'conocimientos para el cumplimiento de la ley, y su trato en'+
+				'lo laboral y personal debe ejercerse sin discriminación'+
+				'alguna y bajo el marco de la libre competencia. El auditor'+
+				'deberá evitar y aprovechar los datos obtenidos de la'+
+				'auditoría para entrar en competencia desleal con'+
+				'profesionales relacionados con ella de otras áreas del'+
+				'conocimiento. Debe apoyar a la empresa a cumplimentar las'+
+				'normas, procedimientos y controles de las auditorias de'+
+				'sistemas de información.'
 			}
 		},
 
